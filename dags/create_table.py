50577ae76fab3e83ea0688b69b46e1059c38a7c2
Jorge Martínez <jorge.ma@wizeline.com>
2022-01-20 11:50:38 -0600

    Fixed datetime import.

--- dags/create_table.py
+++ dags/create_table.py
@@ -1,10 +1,7 @@
-import datetime
+from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
 
 # Default arguments 
 default_args = {
