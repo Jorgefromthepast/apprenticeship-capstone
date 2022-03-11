ac85cf66a86d8fad025e51c4fbb19662ce6c413c
Jorge Martínez <jorge.ma@wizeline.com>
2022-03-11 17:45:03 -0600

    feat: create multiple views
    Adding new views in the dataset at the same time.

--- dags/create_views.py
+++ dags/create_views.py
@@ -25,8 +25,29 @@ with DAG(
     create_view_os = BigQueryCreateEmptyTableOperator(
         task_id="create_view",
         dataset_id="{{ var.value.dataset_id }}",
-        table_id="test_view",
+        table_id="dim_os",
         view=Variable.get("query_os_view", deserialize_json = True)
     )
 
-    create_view_os
+    create_view_devices = BigQueryCreateEmptyTableOperator(
+        task_id="create_view",
+        dataset_id="{{ var.value.dataset_id }}",
+        table_id="dim_devices",
+        view=Variable.get("query_devices_view", deserialize_json = True)
+    )
+
+    create_view_location = BigQueryCreateEmptyTableOperator(
+        task_id="create_view",
+        dataset_id="{{ var.value.dataset_id }}",
+        table_id="dim_location",
+        view=Variable.get("query_location_view", deserialize_json = True)
+    )
+    
+    create_view_browser = BigQueryCreateEmptyTableOperator(
+        task_id="create_view",
+        dataset_id="{{ var.value.dataset_id }}",
+        table_id="dim_browser",
+        view=Variable.get("query_browser_view", deserialize_json = True)
+    )
+    
+    [create_view_os, create_view_devices, create_view_location, create_view_browser]
